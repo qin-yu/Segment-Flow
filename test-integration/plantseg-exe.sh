cd91b7921ced216bac39755afff498bb7e46f022
Qin Yu <qin.yu.95@outlook.com>
2026-02-16 14:21:55 +0000

    test: updated tests for PlantSeg script

--- test-integration/plantseg-exe.sh
+++ test-integration/plantseg-exe.sh
@@ -1,9 +1,10 @@
-python /camp/home/yuq/work/documents/aiod/Segment-Flow/modules/models/resources/usr/bin/run_plantseg2.py \
-    --img-path /camp/home/yuq/work/downloads/N_511_final_crop_ds2_raw.tif \
+python /camp/home/yuq/work/documents/aiod/ai-on-demand/src/ai_on_demand/Segment-Flow/modules/models/resources/usr/bin/run_plantseg2.py \
+    --model-type generic_confocal_3D_unet \
+    --img-path /camp/home/yuq/work/downloads/N_511_final_crop_ds2_raw_crop_small_export.tiff \
     --mask-fname cell_seg \
     --output-dir ./results \
     --model-chkpt dummy \
-    --model-config /camp/home/yuq/work/documents/aiod/Segment-Flow/test-integration/plantseg-config.yaml \
-    --idxs 400 715 500 810 100 260 \
+    --model-config /camp/home/yuq/work/documents/aiod/ai-on-demand/src/ai_on_demand/Segment-Flow/tests/plantseg-config.yaml \
+    --idxs 0 115 0 108 0 80 \
     --channels 1 \
-    --num-slices 260
+    --num-slices 80
